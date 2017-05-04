c684fd73857f70bb0f74364d53a11dc97d4d5135
Eugene Tolmachev <eugene.tolmachev@prolucid.ca>
2017-05-04 15:58:32 -0400

    switching to newer cassandra

--- examples/CassandraAuthSimpleExample/CassandraAuthSimpleExample/Models/IdentityModels.cs
+++
@@ -1,9 +0,0 @@
-﻿using AspNet.Identity.Cassandra.Entities;
-
-namespace CassandraAuthSimpleExample.Models
-{
-    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
-    public class ApplicationUserdfew : CassandraUser
-    {
-    }
-}
--- examples/CastleWindsorExample/CastleWindsorExample/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CastleWindsorExample.MvcApplication" Language="C#" %>
